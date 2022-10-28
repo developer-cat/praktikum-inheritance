75b143d0a81ea348fec8575fb4f21ef37f89388a
developer-cat <99521765+developer-cat@users.noreply.github.com>
2022-10-28 12:05:47 +0700

    Add files via upload

---
+++ Account.java
@@ -0,0 +1,63 @@
+public class Account{
+    protected double balance;
+    
+    public Account(double initBalance){
+        balance = initBalance;
+    }
+    
+    public double getBalance(){
+        return this.balance;
+    }
+    
+    public boolean deposit(double amt){
+        balance += amt;
+        return true;
+    }
+    
+    public boolean withdraw(double amt){
+        balance -= amt;
+        return true;
+    }
+}
+
+class SavingAccount extends Account{
+    private double interestRate;
+    
+    public SavingAccount(double initBalance, double balance, double interest_rate){
+        super(initBalance);
+        super.balance = balance;
+        this.interestRate = interest_rate;
+    }
+}
+
+class CheckingAccount extends Account{
+    private double overdraftProtection;
+    
+    public CheckingAccount(double balance,double protect){
+        super(balance);
+        overdraftProtection = protect;
+    }
+    
+    public CheckingAccount(double balance){
+        super(balance);
+        overdraftProtection =- 1.0;
+    }
+    
+    @Override
+    public boolean withdraw(double amt){
+        double overdraftNeeded = amt - this.balance;
+        if(balance - amt >= 0.0){
+            this.balance -= amt;
+            return true;
+        }
+        else{
+            if(overdraftProtection != -1.0 || overdraftProtection < overdraftNeeded){
+                return false;
+            }else{
+                this.balance = 0.0;
+                overdraftProtection -= overdraftNeeded;
+                return true;
+            }
+        }
+    }
+}
